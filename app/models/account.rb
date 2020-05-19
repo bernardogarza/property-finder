7856c5a795dcf28e2b99326b956c45476971662a
bernardogarza <bernardo.g.landa@gmail.com>
2020-05-19 06:58:23 -0500

    Created db and migrated, association between databases

---
+++ app/models/account.rb
@@ -0,0 +1,10 @@
+class Account < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, 
+         :trackable, :confirmable
+
+  has_many :properties
+  
+end
---
+++ app/models/property.rb
@@ -0,0 +1,3 @@
+class Property < ApplicationRecord
+  belong_to :account 
+end
