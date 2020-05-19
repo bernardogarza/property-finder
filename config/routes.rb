c709777015821d155e66105f6dd108ece10257e8
bernardogarza <bernardo.g.landa@gmail.com>
2020-05-19 09:11:55 -0500

    Worked on links

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
+  get 'dashboard/properties'
+  get 'dashboard/reports'
   resources :properties
   devise_for :accounts
   root to: 'public#main'
